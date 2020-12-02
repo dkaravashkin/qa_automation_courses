b0b5424e8fad79ddbb2dc0c72d633c32e2efe549
Dmitry Karavashkin <dm.karavashkin@gmail.com>
2020-12-02 19:24:18 +0200

    adding java project

---
+++ src/main/java/MyFirstProgram.java
@@ -0,0 +1,27 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        int x = 5, y = 5;
+        final int z = 5;
+
+        boolean boolT = true;
+        boolean boolF = false;
+
+        // -128 to 127
+        byte bt = 127;
+        // -32768 to 32767
+        short shrt = 32767;
+
+        // -2,147,483,648 to 2,147,483,647
+        int integer = 2147483647;
+
+        // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long lng = 9223372036854775807L;
+
+        double dbl = 8.5;
+ 
+        float flt = 2.6F;
+        // 0 to 65535
+        char chr = 'p';
+
+    }
+}
